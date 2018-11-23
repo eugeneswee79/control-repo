5c6db05acaba8cee507cf9e2c8eba02341dd96fd
Eugene Swee <eugeneswee@Eugenes-MacBook-Pro.local>
2018-11-23 15:58:23 +0800

    added new ntp

--- manifests/site.pp
+++ manifests/site.pp
@@ -30,7 +30,7 @@ node default {
   # Example:
   #   class { 'my_class': }
   class {'ntp':
-    servers => ['time.google.com'],
-    enabled => false,
+    servers => ['ntp.ubuntu.com'],
+    enabled => true,
   }
 }
