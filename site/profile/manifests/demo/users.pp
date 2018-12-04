79a5b21c9f1bd8f91c421fd6270c2c82cd9baab4
eugeneswee79 <eugene.swee@puppet.com>
2018-12-04 10:52:29 +0800

    added demo

---
+++ site/profile/manifests/demo/users.pp
@@ -0,0 +1,29 @@
+class profile::demo::users (
+  String $ensure = present,
+
+) {
+
+  case $facts['os']['family'] {
+    'Windows': {
+      user {'win_admin':
+        ensure => $ensure,
+        password => 'secret',
+      }
+    }
+  }
+
+  default: {
+    group {'admins':
+      ensure => $ensure,
+      gid => '888'
+    }
+    user {'myadmin':
+      ensure => $ensure,
+      password => 'secret',
+      shell => '/bin/bash',
+      home => '/home/myadmin',
+      managehome => true,
+      groups => ['admins'],
+    }
+  }
+}
